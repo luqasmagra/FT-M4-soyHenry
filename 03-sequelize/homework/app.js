50da1c5bb3ad56fc9f60168be366c7a1263621d1
Franverri <francoetche@gmail.com>
2020-08-27 15:06:13 -0300

    Only tests missing

--- 03-sequelize/homework/app.js
+++ 03-sequelize/homework/app.js
@@ -7,6 +7,27 @@ db.sync({ force })
     .then(function () {
         app.listen(3001, function () {
             console.log('Server is listening on port 3001!');
+
+          // Descomentar estas lineas:
+          
+          // var catAutos =  Category.create({
+          //   name: "Autos",
+          //   description: "Categoria que habla sobre autos"
+          // });
+          //
+          // var catDeportes =  Category.create({
+          //   name: "Deportes",
+          //   description: "Categoria que habla sobre Deportes"
+          // });
+          //
+          // var catVideojuegos =  Category.create({
+          //   name: "Videojuegos",
+          //   description: "Categoria que habla sobre Videojuegos"
+          // });
+          //
+          // Promise.all([catAutos, catDeportes, catVideojuegos])
+          //   .then(res => {
+          //     console.log("Categorías precargadas");
+          //   });
         });
     });
-
